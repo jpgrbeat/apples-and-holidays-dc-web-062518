cfd726b44690c8bba50041f3c25fb8824af8fd6b
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-18 18:32:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+  holiday_supplies.each do |key,value|
+    puts key
+    value.each do |holiday,supply|
+      puts holiday
+      puts supply.join(" ")
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
